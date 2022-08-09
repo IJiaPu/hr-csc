deb8c13a9071cf27f4993283fd9a2bc20ab3a897
ijiapu <maple@MapledeMacBook-Pro.local>
2022-08-09 14:22:25 +0800

    test

---
+++ src/test/java/com/maple/Test01.java
@@ -0,0 +1,13 @@
+package com.maple;
+
+/**
+ * @author JiaPu
+ * @date 2022/8/8 17:53
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+//        System.out.println();
+        System.out.println(System.nanoTime() - start);
+    }
+}
